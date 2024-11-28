ef5a36354e5f5a08423ace4813e1fda1c5f86842
Barun Karmakar <kumarbarun137@gmail.com>
2024-11-28 11:41:50 +0530

    Update Project.js

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 import { Box, Container, Typography } from "@mui/material";
 import { MUIStyle } from "./MUIStyle";
 import ProjectBox from "../ProjectBox/ProjectBox";
@@ -9,40 +9,31 @@ import { useGSAP } from '@gsap/react';
 export default function Project() {
   const projectBoxes = Array(5).fill(null); // Adjust the number of ProjectBox components as needed
 
+  const projectSecRef = useRef(null);
+
   // Runs before the first render
   useLayoutEffect(() => {
     gsap.registerPlugin(useGSAP, ScrollTrigger);
   }, []);
 
   useGSAP(() => {
-    let cards = gsap.utils.toArray(".card");
+    const cards = gsap.utils.toArray('.card__inner');
 
-    cards.forEach((card, index) => {
-      const cardInner = card.querySelector(".card__inner");
+    console.log("cards", cards);
 
-      gsap.fromTo(card,
-        { y: 100, opacity: 1, marker: true },
-        {
-          y: 0,
-          opacity: 1,
-          scrollTrigger: {
-            trigger: card,
-            start: "top center",
-            end: "top center",
-            scrub: true,
-            toggleActions: "play none none reverse",
-            markers: true,  // Set to true to see the trigger area
-          }
-        },
-      );
-    });
 
-    ScrollTrigger.create({
-      trigger: ".projectSec",
-      start: "top top",
-      end: () => "+=" + document.querySelector(".projectSec").offsetHeight * cards.length,
-      pin: true,
-      pinSpacing: true
+    cards.forEach((card, i) => {
+      ScrollTrigger.create({
+        trigger: card, // The element that ScrollTrigger will "trigger" when it comes into view
+        start: "top top", // When the top of the trigger hits the top of the viewport
+        end: "top bottom", // When the top of the trigger hits the bottom of the viewport
+        endTrigger: projectSecRef.current, // The element that ScrollTrigger will use as the end of the trigger.
+        pin: true, // Pin the trigger element while it is in view
+        pinSpacing: false, // Don't reserve the pin element's space in the document
+        markers: true, // Show markers for testing purposes
+        scrub: true, // Smoothly animate the pin movement
+        anticipatePin: 1 // Wait for 1 second before pinning the element
+      });
     });
   });
 
@@ -60,13 +51,15 @@ export default function Project() {
           </Box>
         </Box>
 
-        <Box className="projectSec" sx={{ ...MUIStyle.ProjectSec, }}>
+        <Box className="projectSec" sx={{ ...MUIStyle.ProjectSec, }} ref={projectSecRef}>
           {projectBoxes.map((_, index) => (
-            <div key={index} className="card"
-              style={{}}
-            >
-              <ProjectBox />
-            </div>
+            // <div key={index} className="card"
+            //   style={{}}
+            // >
+            <ProjectBox
+              key={index}
+            />
+            // </div>
           ))}
         </Box>
       </Container>
