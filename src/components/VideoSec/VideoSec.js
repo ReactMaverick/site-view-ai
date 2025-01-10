45070f88670cd60126446c4b7c19f52c36576f80
Barun Karmakar <kumarbarun137@gmail.com>
2025-01-10 17:17:44 +0530

    Update VideoSec.js

--- src/components/VideoSec/VideoSec.js
+++ src/components/VideoSec/VideoSec.js
@@ -1,3 +1,4 @@
+'use client';
 import { Box, Button, Typography } from "@mui/material";
 import { MUIStyle } from "./MUIStyle";
 import { VIDEOBG1, VIDEOICON } from "@/values/Constants/ImageConstants";
