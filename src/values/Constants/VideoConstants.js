6b21c55910553ef33d37414bafdeeafa110cacf9
SaikatRuj <saikat.websadroit@gmail.com>
2025-06-20 14:51:06 +0530

    20-06-2025

--- src/values/Constants/VideoConstants.js
+++ src/values/Constants/VideoConstants.js
@@ -4,5 +4,5 @@ const URL = (url) => {
     return '/videos/' + url;
 }
 
-export const CONNECTEDCONSTRUCTION = URL('connected-construction.mp4');
+export const CONNECTEDCONSTRUCTION = URL('connected-construction-2.mp4');
 // export const CONNECTEDCONSTRUCTION = URL('/videos/connected-construction.mp4'); UNCOMMENT ON PRODUCTION
