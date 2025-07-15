a253aece56995239a1d5c35ee14f4b50365f7b87
Saikat Ruj <164467233+SaikatRuj@users.noreply.github.com>
2025-07-15 16:05:19 +0530

    15-07-2025

--- src/components/BlogGrid/BlogGrid.js
+++ src/components/BlogGrid/BlogGrid.js
@@ -1,23 +1,46 @@
 "use client";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography, CircularProgress } from "@mui/material";
 import { MUIStyle } from "./MUIStyle";
-import React from "react";
+import React, { useEffect } from "react";
 import BlogItem from "../BlogItem/BlogItem";
-import { BlogContent } from "./BlogContent";
+import { useDispatch, useSelector } from "react-redux";
+import { SelectBlogsLoading, getAllBlogs } from "@/redux/blogSlice";
 
 export default function BlogGrid() {
+  const dispatch = useDispatch();
+  const blogs = useSelector((state) => state.blog.blogs);
+  const blogsLoading = useSelector(SelectBlogsLoading);
+
+  useEffect(() => {
+    dispatch(getAllBlogs());
+  }, [dispatch]);
+
+  if (blogsLoading) {
+    return (
+      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "40vh" }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (!blogsLoading && blogs.length === 0) {
+    return (
+      <Box sx={{ textAlign: "center", padding: "20px" }}>
+        <Typography variant="h6">No blogs available</Typography>
+      </Box>
+    );
+  }
+
   return (
     <Box sx={MUIStyle.SingleBlogMain}>
       <Container maxWidth="xl">
         <Box sx={MUIStyle.SingleBlogGrid}>
-          {BlogContent.map((item, i) => (
+          {blogs.map((item, i) => (
             <BlogItem
-              key={i}
+              key={item.id || i}
               title={item.title}
-              desc={item.desc}
-              btn={item.btn}
-              img={item.img}
-              slug={item.slug}
+              desc={item.short_description}
+              img={item.banner_image}
+              slug={item.id}
             />
           ))}
         </Box>
